b1a5e1e5d16feea0c0d7ec76d6019a1a98fda8c2
leeh.peter <leeh.peter@zengo.eu>
2022-02-14 09:21:16 +0100

    some improvements: add 3 sensor

--- src/lib.rs
+++ src/lib.rs
@@ -21,13 +21,13 @@ use shape::Circle;
 
 #[pymodule]
 fn place_me(_py: Python, m: &PyModule) -> PyResult<()> {
-    // m.add_function(wrap_pyfunction!(run, m)?)?;
-    m.add_function(wrap_pyfunction!(run_v2, m)?)?;
+    m.add_function(wrap_pyfunction!(optimize_v2, m)?)?;
+    m.add_function(wrap_pyfunction!(optimize_v3, m)?)?;
     Ok(())
 }
 
 #[pyfunction]
-fn run_v2<'py>(
+fn optimize_v2<'py>(
     _py: Python<'_>,
     xs: Vec<f64>,
     ys: Vec<f64>,
@@ -40,6 +40,23 @@ fn run_v2<'py>(
     let (x_range, y_range) = Sensor::coordinates_along_circumference(width, height, pixel_step);
     let circles = initialize_circles(&xs, &ys, &radii);
     let v = inner_calculate_v2(circles, x_range, y_range, resolution, width, height);
+    Ok(v)
+}
+
+#[pyfunction]
+fn optimize_v3<'py>(
+    _py: Python<'_>,
+    xs: Vec<f64>,
+    ys: Vec<f64>,
+    radii: Vec<f64>,
+    width: u32,
+    height: u32,
+    resolution: u32,
+    pixel_step: usize,
+) -> PyResult<Vec<f64>> {
+    let (x_range, y_range) = Sensor::coordinates_along_circumference(width, height, pixel_step);
+    let circles = initialize_circles(&xs, &ys, &radii);
+    let v = inner_calculate_v3(circles, x_range, y_range, resolution, width, height);
     Ok(v)
 }
 
@@ -64,7 +81,6 @@ fn inner_calculate_v2(
     let report = Arc::new(Mutex::new(Report {
         max_coverage: 0.0,
         sensor_positions: Vec::new(),
-        extra: Vec::new(),
     }));
 
     let full_circle: f64 = 2.0 * std::f64::consts::PI;
@@ -158,12 +174,7 @@ fn inner_calculate_v2(
                     point::Point::new(x as f64, y as f64),
                     point::Point::new(x2 as f64, y2 as f64),
                 ];
-            // TODO: floating-point arithmetic, it's kind of dumb to check for equality
-            } else if cov == result.max_coverage {
-                result.extra.push(vec![
-                    point::Point::new(x as f64, y as f64),
-                    point::Point::new(x2 as f64, y2 as f64),
-                ]);
+                // TODO: floating-point arithmetic, it's kind of dumb to check for equality
             }
             field_res.circles = restore.clone();
         });
@@ -184,112 +195,134 @@ fn inner_calculate_v2(
     ])
 }
 
-// Old implementation, ignore.
-// fn inner_calculate(
-//     circles: Vec<Circle>,
-//     x_range: Vec<u32>,
-//     y_range: Vec<u32>,
-//     resolution: u32,
-//     width: u32,
-//     height: u32,
-// ) -> Vec<f64> {
-//     let report = Arc::new(Mutex::new(Report {
-//         max_coverage: 0.0,
-//         sensor_positions: Vec::new(),
-//         extra: Vec::new(),
-//     }));
+fn inner_calculate_v3(
+    circles: Vec<Circle>,
+    x_range: Vec<u32>,
+    y_range: Vec<u32>,
+    resolution: u32,
+    width: u32,
+    height: u32,
+) -> Vec<f64> {
+    let report = Arc::new(Mutex::new(Report {
+        max_coverage: 0.0,
+        sensor_positions: Vec::new(),
+    }));
 
-//     let full_circle: f64 = 2.0 * std::f64::consts::PI;
+    let full_circle: f64 = 2.0 * std::f64::consts::PI;
 
-//     let full_arclength = full_circle * circles.len() as f64;
+    let full_arclength = full_circle * circles.len() as f64;
 
-//     x_range.par_iter().zip(y_range.clone()).for_each(|(&x, y)| {
-//         let sensor =
-//             Sensor::new_at(&point::Point::new(x as f64, y as f64)).with_resolution(resolution);
-//         let rays = sensor.rays.clone();
-//         let mut field = Field::new(circles.clone(), resolution, width, height);
+    x_range.par_iter().zip(y_range.clone()).for_each(|(&x, y)| {
+        let sensor =
+            Sensor::new_at(&point::Point::new(x as f64, y as f64)).with_resolution(resolution);
+        let rays = sensor.rays.clone();
+        let field = Field::new(circles.clone(), resolution, width, height);
+        let mut field_res = field.clone();
 
-//         for ray in rays {
-//             cast_ray(&mut field, &ray);
-//         }
+        rays.windows(2).for_each(|pair| {
+            let i1 = cast_ray(&field, &pair[0]);
+            let i2 = cast_ray(&field, &pair[1]);
+            if let Some(elem1) = i1 {
+                if let Some(elem2) = i2 {
+                    if elem1.id == elem2.id {
+                        let range = elem1.get_range_for_ray_pair(&pair[0], &pair[1]);
+                        field_res.update_stack(elem1.id, range);
+                    }
+                }
+            }
+        });
 
-//         let restore = field.circles.clone();
+        let restore = field_res.circles.clone();
 
-//         x_range.iter().zip(y_range.clone()).for_each(|(&x2, y2)| {
-//             let sensor2 = Sensor::new_at(&point::Point::new(x2 as f64, y2 as f64))
-//                 .with_resolution(resolution);
-//             let rays2 = sensor2.rays.clone();
+        x_range.iter().zip(y_range.clone()).for_each(|(&x2, y2)| {
+            let sensor2 = Sensor::new_at(&point::Point::new(x2 as f64, y2 as f64))
+                .with_resolution(resolution);
+            let rays2 = sensor2.rays.clone();
 
-//             for ray in rays2 {
-//                 cast_ray(&mut field, &ray);
-//             }
+            rays2.windows(2).for_each(|pair| {
+                let i1 = cast_ray(&field, &pair[0]);
+                let i2 = cast_ray(&field, &pair[1]);
+                if let Some(elem1) = i1 {
+                    if let Some(elem2) = i2 {
+                        if elem1.id == elem2.id {
+                            let range = elem1.get_range_for_ray_pair(&pair[0], &pair[1]);
+                            field_res.update_stack(elem1.id, range);
+                        }
+                    }
+                }
+            });
 
-//             let covered: f64 = field
-//                 .circles
-//                 .iter()
-//                 .map(|circle| {
-//                     circle
-//                         .range_stack
-//                         .ranges
-//                         .par_iter()
-//                         .collect::<RangeStack>()
-//                         .length()
-//                 })
-//                 .sum();
-//             let mut result = report.lock().unwrap();
-//             if covered > result.max_coverage {
-//                 result.max_coverage = covered;
-//                 result.sensor_positions = vec![
-//                     point::Point::new(x as f64, y as f64),
-//                     point::Point::new(x2 as f64, y2 as f64),
-//                 ];
-//             } else if covered == result.max_coverage {
-//                 result.extra.push(vec![
-//                     point::Point::new(x as f64, y as f64),
-//                     point::Point::new(x2 as f64, y2 as f64),
-//                 ]);
-//             }
-//             //     println!(
-//             //         "percentage covered {:?} at ({:?}, {:?}), other at ({:?}, {:?})",
-//             //         100.0 * covered / full_arclength,
-//             //         x2,
-//             //         y2,
-//             //         x,
-//             //         y,
-//             //     );
-//             field.circles = restore.clone();
-//         });
-//     });
-//     let rep = report.lock().unwrap();
-//     if rust_log_is_set() {
-//         rep.pprint(full_arclength);
-//     }
-//     // This is safe, because we have two points as result.
-//     Vec::from([
-//         rep.sensor_positions[0].x,
-//         rep.sensor_positions[0].y,
-//         rep.sensor_positions[1].x,
-//         rep.sensor_positions[1].y,
-//     ])
-// }
+            let restore2 = field_res.circles.clone();
 
-// #[pyfunction]
-// fn run<'py>(
-//     _py: Python<'_>,
-//     xs: Vec<f64>,
-//     ys: Vec<f64>,
-//     radii: Vec<f64>,
-//     width: u32,
-//     height: u32,
-//     resolution: u32,
-//     pixel_step: usize,
-// ) -> PyResult<Vec<f64>> {
-//     let (x_range, y_range) = Sensor::coordinates_along_circumference(width, height, pixel_step);
+            x_range.iter().zip(y_range.clone()).for_each(|(&x3, y3)| {
+                let sensor3 = Sensor::new_at(&point::Point::new(x3 as f64, y3 as f64))
+                    .with_resolution(resolution);
+                let rays3 = sensor3.rays.clone();
 
-//     let circles = initialize_circles(&xs, &ys, &radii);
-//     let v = inner_calculate(circles, x_range, y_range, resolution, width, height);
-//     Ok(v)
-// }
+                rays3.windows(2).for_each(|pair| {
+                    let i1 = cast_ray(&field, &pair[0]);
+                    let i2 = cast_ray(&field, &pair[1]);
+                    if let Some(elem1) = i1 {
+                        if let Some(elem2) = i2 {
+                            if elem1.id == elem2.id {
+                                let range = elem1.get_range_for_ray_pair(&pair[0], &pair[1]);
+                                field_res.update_stack(elem1.id, range);
+                            }
+                        }
+                    }
+                });
+                let covered_len = field_res
+                    .circles
+                    .iter()
+                    .map(|circle| {
+                        circle
+                            .range_stack
+                            .ranges
+                            .par_iter()
+                            .collect::<RangeStack>()
+                            .length()
+                    })
+                    .sum::<f64>()
+                    / full_arclength;
+
+                let seen = field_res
+                    .clone()
+                    .circles
+                    .iter()
+                    .take_while(|circle| !circle.range_stack.is_empty())
+                    .count();
+
+                let cov = seen as f64 + covered_len * circles.len() as f64;
+
+                let report = report.clone();
+                let mut result = report.lock().unwrap();
+                if cov > result.max_coverage {
+                    result.max_coverage = cov;
+                    result.sensor_positions = vec![
+                        point::Point::new(x as f64, y as f64),
+                        point::Point::new(x2 as f64, y2 as f64),
+                        point::Point::new(x3 as f64, y3 as f64),
+                    ];
+                }
+                field_res.circles = restore2.clone();
+            });
+            field_res.circles = restore.clone();
+        });
+    });
+
+    let rep = report.lock().unwrap();
+    if rust_log_is_set() {
+        rep.pprint(circles.len());
+    }
+    Vec::from([
+        rep.sensor_positions[0].x,
+        rep.sensor_positions[0].y,
+        rep.sensor_positions[1].x,
+        rep.sensor_positions[1].y,
+        rep.sensor_positions[2].x,
+        rep.sensor_positions[2].y,
+    ])
+}
 
 fn rust_log_is_set() -> bool {
     match std::env::var("RUST_LOG") {
--- src/report.rs
+++ src/report.rs
@@ -1,5 +1,3 @@
-use std::num;
-
 // TODO: This is kind of bad, some rework needed..
 use crate::point::Point;
 
@@ -7,13 +5,16 @@ use crate::point::Point;
 pub struct Report {
     pub max_coverage: f64,
     pub sensor_positions: Vec<Point>,
-    pub extra: Vec<Vec<Point>>,
 }
 
 impl Report {
     pub fn pprint(&self, num_circles: usize) {
-        println!("\ncovered {:?}/{:?}", self.max_coverage, num_circles * 2);
+        println!(
+            "\n🎯 covered {:?}% ({:?}/{:?})",
+            self.max_coverage / (num_circles * 2) as f64 * 100.0,
+            self.max_coverage,
+            num_circles * 2
+        );
         println!("optimal positions {:#?}", self.sensor_positions);
-        println!("also at {:#?}", self.extra);
     }
 }
